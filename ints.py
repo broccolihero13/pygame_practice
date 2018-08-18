5696843b64285ea10fc5e3b2ad7d237b13913aa7
Brock Halladay <bhalladay@instructure.com>
2018-08-18 11:29:46 -0600

    practice

---
+++ ints.py
@@ -0,0 +1,13 @@
+intNumber = 1
+floatNumber = 1.0
+negNumber = -2
+negFloat = -2.0
+
+print(negFloat + floatNumber + 10)
+
+print(intNumber % 2)
+print(intNumber % 2 == 0)
+print(intNumber + floatNumber)
+print(intNumber * floatNumber)
+
+print(34 ** 2)
--- vars.py
+++ vars.py
@@ -1,6 +1,7 @@
 name = "Brock"
-
-
 name = "Rose"
 
 print(name) # prints "Rose" because the variable was overwritten
+
+print(name[:0:-1])
+print(f"first name: {name}")
