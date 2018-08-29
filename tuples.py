6fd3a9c1aa37e42ac1b175e5808abe5918cf77e2
Brock Halladay <bhalladay@instructure.com>
2018-08-29 13:12:57 -0600

    practice with tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+import pygame
+# pygame.init()
+
+# print((1,2,3,4,5,6,6,6))
+
+screen_size = (900,700) #width,height
+print(screen_size)
+print(f"width is: {screen_size[0]} \nheight is: {screen_size[1]}")
+
+my_tuple = (1,2)
+print(my_tuple)
