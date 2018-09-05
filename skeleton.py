78a65455a179b6d4303bab70270100a75ed3ae5a
Brock Halladay <bhalladay@instructure.com>
2018-09-05 13:18:13 -0700

    Pygame Skeleton set up

--- skeleton.py
+++ skeleton.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 
 pygame.init()
 
@@ -7,9 +7,15 @@ height = 700
 screenDim = (width,height)
 
 screen = pygame.display.set_mode(screenDim)
+pygame.display.set_caption("Game Name Here")
 finished = False
 
 while not finished:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+            # pygame.quit()
         
     pygame.display.flip()
+
+pygame.quit()
