1e1a72284c2fb34d79243963b83d765e9f744ef7
Brock Halladay <bhalladay@instructure.com>
2018-09-05 13:01:19 -0700

    skeleton still crashes, but is running through each event in main loop

--- skeleton.py
+++ skeleton.py
@@ -6,3 +6,10 @@ width = 900
 height = 700
 screenDim = (width,height)
 
+screen = pygame.display.set_mode(screenDim)
+finished = False
+
+while not finished:
+    for event in pygame.event.get():
+        
+    pygame.display.flip()
