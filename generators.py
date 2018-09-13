cf9a6a90a0d004181dc02211ee582489641bb27b
Brock Halladay <bhalladay@instructure.com>
2018-09-13 12:46:52 -0500

    generator practice

--- generators.py
+++ generators.py
@@ -1,10 +1,10 @@
 import random
 
-def NumberGenerator(my_str, i):
+def number_generator(my_str, i):
     for i in range(random.randint(1, i)):
         yield (my_str,i)
 
-startNum = NumberGenerator("First", 15)
+startNum = number_generator("First", 15)
 
 print(list(startNum))
 
@@ -12,3 +12,14 @@ print(list(startNum))
 name_list = ['Brock', 'Monique', 'Brian', 'Francesqua', 'Alexis', 'Shirley']
 
 print(list(name.upper() for name in name_list))
+
+# generator object
+def odd_number_generator(n):
+    for i in range(n):
+        if i % 2 != 0:
+            yield i
+
+odd_ints = odd_number_generator(20)
+odd_ints.__next__()
+odd_ints.__next__()
+print(odd_ints.__next__())
