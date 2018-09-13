e94d005830bade032dfea4a96dba885d8e6412f3
Brock Halladay <bhalladay@instructure.com>
2018-09-13 12:34:00 -0500

    practice with generator expression

--- generators.py
+++ generators.py
@@ -7,3 +7,8 @@ def NumberGenerator(my_str, i):
 startNum = NumberGenerator("First", 15)
 
 print(list(startNum))
+
+# generator expression
+name_list = ['Brock', 'Monique', 'Brian', 'Francesqua', 'Alexis', 'Shirley']
+
+print(list(name.upper() for name in name_list))
